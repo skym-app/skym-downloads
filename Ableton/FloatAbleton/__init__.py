272e991831503863420ad2513dd8ea654cc3ea9b
Connor Golden <cgolden@moogmusic.com>
2020-03-16 19:58:47 -0400

    add float ableton

---
+++ Ableton/FloatAbleton/__init__.py
@@ -0,0 +1,4 @@
+from FLOAT import FLOAT
+
+def create_instance(c_instance):
+    return FLOAT(c_instance)
--- Ableton/FloatationScript/__init__.py
+++ Ableton/FloatationScript/__init__.py
@@ -1,3 +1,5 @@
+
+from __future__ import absolute_import, print_function, unicode_literals
 from FLOAT import FLOAT
 
 def create_instance(c_instance):
